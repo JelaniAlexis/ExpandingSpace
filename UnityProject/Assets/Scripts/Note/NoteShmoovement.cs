e9f0d2320b231757b2e683db34b31b99802eeeec
Jayden <macbeanjayden@gmail.com>
2021-05-19 14:32:58 +0200

    Note movement added

---
+++ UnityProject/Assets/Scripts/Note/NoteShmoovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteShmoovement : MonoBehaviour
+{
+    public GameObject Note;
+    public float Speed = -7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Note = GameObject.Find("Note");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Speed * Time.deltaTime, 0, 0);
+    }
+}
